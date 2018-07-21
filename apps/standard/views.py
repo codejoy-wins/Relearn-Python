40a24859a65cbd2ccd9c6a4921c0473418e2822a
Max <mjann@ucsc.edu>
2018-07-20 19:38:47 -0700

    styled homepage

--- apps/standard/views.py
+++ apps/standard/views.py
@@ -3,4 +3,4 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 def odell(request):
-    return redirect('/blogs/')
+    return render(request, 'standard/index.html')
