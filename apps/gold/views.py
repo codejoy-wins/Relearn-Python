1f6fb790d737a9406dde25f4861d04e03c900ce6
Max <mjann@ucsc.edu>
2018-07-26 04:00:03 -0700

    fixed ninjagold

--- apps/gold/views.py
+++ apps/gold/views.py
@@ -4,45 +4,52 @@ from django.shortcuts import render, HttpResponse, redirect
 import random
 
 def index(request):
-    print "NINJA GOLD BABY"
-    print request.session['gold_now']
+    if 'color_now' in request.session:
+        print request.session['color_now']
+        if request.session['color_now'] == "green":
+            print "green logic"
+            logic = "gained"
+        else:
+            print "red logic"
+            logic = "lost"
+
     context = {
         "jay": "silent bob",
         "total_gold": request.session['gold'],
-        "gold_now": request.session['gold_now']
+        "gold_now": request.session['gold_now'],
+        "my_color": request.session['color_now'],
+        "my_logic": logic
     }
     return render(request, 'gold/index.html', context)
 
 def process(request, location):
     print 'processing'
     print location
+    color = "green"
     if location == 'farm':
-        print "You stole 10-20 gold"
         gold = random.randrange(10,21)
-        print gold
 
     if location == 'cave':
-        print "You stole 5-10 gold"
         gold = random.randrange(5,11)
-        print gold
     if location == 'house':
-        print "You stole 2-5 gold"
         gold = random.randrange(2,6)
-        print gold
     if location == 'casino':
-        print "You won or lost up to 50 gold"
         gold = random.randrange(-50,51)
-        print gold
+        if gold <0:
+            color = "red"
 
     print "Gold is", gold
+    print "Color is", color
 
     if not 'gold' in request.session:
         request.session['gold'] = 0
     request.session['gold'] += gold
     request.session['gold_now'] = gold
+    request.session['color_now'] = color
 
     print request.session['gold'], "is your total gold"
     print request.session['gold_now'], "gold added"
+    print request.session['color_now'], "color now"
 
     return redirect('/gold/')
 
